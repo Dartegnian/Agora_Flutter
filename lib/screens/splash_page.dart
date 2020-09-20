fbaadf3979ec92c98387b29c883fab7f62ebff43
Dartegnian <Dartegnian@users.noreply.github.com>
2020-09-20 23:22:30 +0800

    Remove basic splash page and add initial splash screen design

--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -4,8 +4,60 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Agora splash page"),
+      body: Container(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/splash/splash_bg.jpg"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Color(0xFFfBBF00),
+                    BlendMode.color,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    offset: Offset.fromDirection(20.0),
+                    blurRadius: 10.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.25,
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logos/agora_wordmark.png',
+                    width: MediaQuery.of(context).size.width * 0.75,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                  ),
+                  Text(
+                    "Agora splash page",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
